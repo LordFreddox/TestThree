8f4359d40fb567367d30a01ee4cdc7b483f76a5e
Freddy Garcia <fjgv95@gmail.com>
2024-07-04 18:21:48 -0500

    Acrtualizacion de avatar

---
+++ src/fluttertothree.js
@@ -0,0 +1,11 @@
+function InitAudio(){
+
+}
+
+function ReturnFlutterPermission(){
+    playButton.addEventListener("click", FuncionTest);
+
+function FuncionTest(){
+  console.log("se presiono el boton");
+}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
 //import Stats from 'three/addons/libs/stats.module.js'
-const welcomeScreen = (document.getElementById('boton') as HTMLFormElement);
-welcomeScreen.addEventListener("click", InitAudio);
+const playButton = (document.getElementById('boton') as HTMLFormElement);
+
+
+declare function sendMessageWindow(): void;
+
 
 const scene = new THREE.Scene()
 
@@ -26,10 +29,7 @@ const scene = new THREE.Scene()
 }*/
 
 var getUrlParameter = function getUrlParameter(sParam: string) {
-  var sPageURL = window.location.search.substring(1),
-      sURLVariables = sPageURL.split('&'),
-      sParameterName,
-      i;
+  var sPageURL = window.location.search.substring(1), sURLVariables = sPageURL.split('&'), sParameterName, i;
 
   for (i = 0; i < sURLVariables.length; i++) {
       sParameterName = sURLVariables[i].split('=');
@@ -41,7 +41,8 @@ var getUrlParameter = function getUrlParameter(sParam: string) {
   return false;
 };
 
-var model = getUrlParameter("BigSurfaceId");
+var bigSurface = String(getUrlParameter("BigSurfaceId"));
+var model = bigSurface.split("-", 3); 
 
 new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping
@@ -65,10 +66,6 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
-controls.target.set(0, 0.5, 0)
-
 /*const stats = new Stats()
 document.body.appendChild(stats.dom)*/
 
@@ -78,33 +75,22 @@ document.body.appendChild(stats.dom)*/
 // }
 
 let mixer: THREE.AnimationMixer
-//let animationActions: { [key: string]: THREE.AnimationAction } = {}
-//let activeAction: THREE.AnimationAction
-// let speed = 0,
-//   toSpeed = 0
 
-/*const listener = new THREE.AudioListener();
-camera.add( listener );
 
-// create a global audio source
-const sound = new THREE.Audio( listener );
-
-// load a sound and set it as the Audio object's buffer
-const audioLoader = new THREE.AudioLoader();
-audioLoader.load('sounds/avatar.mp3', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( true );
-	sound.setVolume( 0.5 );
-	sound.play();
-  console.log("se cargó el audio");
-});*/
-
-new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/AVATAR%20AR%20EXPERIENCIA%203D/models/'+model+'.glb', (gltf) => {
+new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/ThreeJSProduction/ThreeAvatarFlutter/models/'+model[0]+'.glb', (gltf) => {
   gltf.scene.scale.set(0.6, 0.6, 0.6); 
   mixer = new THREE.AnimationMixer(gltf.scene)
   //   //console.log(gltf  )
-  mixer.clipAction(gltf.animations[1]).play()
+  const action = mixer.clipAction(gltf.animations[0]);
+  action.setLoop(THREE.LoopOnce, 1);  // Configura la animación para que no se repita
+  action.clampWhenFinished = true; //Detener al finalizar
+  //mixer.clipAction(gltf.animations[0]).play()
   scene.add(gltf.scene)
+
+  playButton.addEventListener('click', () => {
+    action.play();
+    InitAudio();
+  });
 });
 console.log("modelo:"+model+"cargo");
  // Cargar y reproducir el audio
@@ -116,29 +102,15 @@ console.log("modelo:"+model+"cargo");
  
 
  function InitAudio(){
-  audioLoader.load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/AVATAR%20AR%20EXPERIENCIA%203D/sounds/'+model+'.mp3', function(buffer) {
+  audioLoader.load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/ThreeJSProduction/ThreeAvatarFlutter/sounds/'+model[0]+'.mp3', function(buffer) {
     sound.setBuffer(buffer);
     sound.setLoop(false);
     sound.setVolume(0.5);
     sound.play();
-    welcomeScreen.style.display = 'none';
+    playButton.style.display = 'none';
 });
+    sendMessageWindow();
  }
-/*
-if(model=="100"){
-//new GLTFLoader().load('models/mascara.glb', (gltf) => {
-new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/AVATAR%20AR%20EXPERIENCIA%203D/models/man.glb', (gltf) => {
-  gltf.scene.scale.set(0.6, 0.6, 0.6); 
-  mixer = new THREE.AnimationMixer(gltf.scene)
-  //   //console.log(gltf  )
-  mixer.clipAction(gltf.animations[1]).play()
-  scene.add(gltf.scene)
-})
-console.log("El Dorado");
-}
-else{
-  console.log("No trajo Modelo");
-}*/
 const clock = new THREE.Clock()
 let delta = 0
 
@@ -147,9 +119,9 @@ function animate() {
 
   delta = clock.getDelta()
 
-  controls.update()
 
   mixer.update(delta)
+
   renderer.render(scene, camera)
 
 }
