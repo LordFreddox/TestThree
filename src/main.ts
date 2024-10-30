dec8f1c95888b2051059f21af78cf5e1885f6e6f
Freddy Garcia <fjgv95@gmail.com>
2024-10-30 10:51:24 -0500

    Ultimos cambios de Avatar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import * as TWEEN from '@tweenjs/tween.js';
+//import * as TWEEN from '@tweenjs/tween.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import gsap from 'gsap';
 // Crear la escena, cámara y renderer
@@ -9,16 +9,17 @@ const playButton = (document.getElementById('boton') as HTMLFormElement);
 const logo = (document.getElementById('logo') as HTMLFormElement);
 var targetOrigin:string;
 //var buttonPosition = new THREE.Vector3(80, 10, 0);
-var centerPosition = new THREE.Vector3(0.1, -1, 0);
-var originPointIn3D= new THREE.Vector3(0.1, 2, -5);
+//var centerPosition = new THREE.Vector3(0.1, -1, 0);
+//var originPointIn3D= new THREE.Vector3(0.1, 2, -5);
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
 renderer.setClearColor(0x000000, 0);
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.localClippingEnabled = true // Habilitar clipping
 document.body.appendChild(renderer.domElement)
 let mixer: THREE.AnimationMixer
+
 
 const controls = new OrbitControls(camera, renderer.domElement)
 camera.position.set(0, 2, 5)
@@ -123,7 +124,7 @@ new RGBELoader().load('img/venice_sunset_1k.hdr', (texture) => {
 const clipPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0) // Dirección inicial del plano de recorte
 const clipPlanes = [clipPlane]
 
-new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/ThreeJSProduction/ThreeAvatarFlutter/models/'+model[0]+'.glb', (gltf) => {
+new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/ThreeJSProduction/ThreeAvatarPortalTest/models/'+model[0]+'.glb', (gltf) => {
   mixer = new THREE.AnimationMixer(gltf.scene)
   const action1 = mixer.clipAction(gltf.animations[0])
   const action2 = mixer.clipAction(gltf.animations[1])
@@ -143,10 +144,24 @@ new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/Un
 
   mixer.addEventListener('finished', async () => {
     console.log('La animación ha terminado')
-    await animateModelToPosition(gltf.scene, originPointIn3D)
-    shrinkModelToDisappear(gltf.scene).then(() => {
-      console.log('El modelo ha desaparecido.')
-    })
+    gsap.fromTo(
+      clipPlane,
+      { constant: 1}, // Comienza desde la parte inferior
+      {
+        constant: -5,
+        duration: 5, // Ajusta esta duración para controlar la velocidad
+        ease: 'power1.inOut',
+        onUpdate: () => {
+          scene.traverse((child) => {
+            if (child instanceof THREE.Mesh) {
+              child.material.clippingPlanes = clipPlanes
+            }
+          })
+          renderer.render(scene, camera)
+        }
+      }
+    )
+    
   })
 
   scene.traverse((child) => {
@@ -163,6 +178,7 @@ new GLTFLoader().load('https://sasiteit.blob.core.windows.net/container-unity/Un
     InitAudio()
     updateAndSendMessage('GesturesCatched', '')
     console.log("reproduce animación");
+    playButton.style.display = 'none';
   })
 
   // Animar el plano de recorte de abajo hacia arriba después de cargar el modelo
@@ -207,99 +223,6 @@ window.addEventListener('resize', () => {
 })
 
 
-
-
-function animateModelToPosition(model: THREE.Object3D, targetPosition: THREE.Vector3) {
-  const startPosition = model.position.clone();
-  new TWEEN.Tween(startPosition)
-    .to(targetPosition, 2000) // 1000ms = 1 segundo
-    .easing(TWEEN.Easing.Quadratic.InOut) // Usar una función de suavizado
-    .onUpdate(() => {
-      model.position.copy(startPosition);
-    })
-    .start();
-}
-
-//const raycaster = new THREE.Raycaster();
-//const mouse = new THREE.Vector2();
-/*function getPointIn3DFromRelativeCoordinates(xPercentage: number, yPercentage: number): THREE.Vector3 {
-  // Calcular las coordenadas NDC
-  mouse.x = (xPercentage / window.innerWidth) * 2 - 1;
-  mouse.y = (yPercentage / window.innerHeight) * 2 - 1;
-
-  // Actualizar el raycaster con las coordenadas del mouse y la cámara
-  raycaster.setFromCamera(mouse, camera);
-  console.log(scene.children);
-  // Intersecar el rayo con un plano en el espacio 3D
-  const plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-  const pointIn3D = new THREE.Vector3();
-  raycaster.ray.intersectPlane(plane, pointIn3D);
-  console.log(scene.children);
-  return pointIn3D;
-}*/
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function shrinkModelToDisappear(model: THREE.Object3D): Promise<void> {
-  return new Promise((resolve) => {
-    const startScale = model.scale.clone();
-    const endScale = new THREE.Vector3(0, 0, 0);
-
-    new TWEEN.Tween(startScale)
-      .to(endScale, 2000) // 1000ms = 1 segundo
-      .easing(TWEEN.Easing.Quadratic.InOut) // Usar una función de suavizado
-      .onUpdate(() => {
-        model.scale.copy(startScale);
-      })
-      .onComplete(() => {
-        //scene.remove(model); // Opcional: Remover el modelo de la escena
-        resolve();
-      })
-      .start();
-  });
-}
-function growModelToAppear(model: THREE.Object3D, duration: number = 2000) {
-  const targetScale = new THREE.Vector3(1, 1, 1);
-  const startScale = new THREE.Vector3(0.01, 0.01, 0.01); // Tamaño inicial pequeño
-
-  // Establecer el tamaño inicial del modelo
-  model.scale.copy(startScale);
-
-  // Animar el crecimiento del modelo hasta el tamaño objetivo
-  new TWEEN.Tween(model.scale)
-    .to(targetScale, duration)
-    .easing(TWEEN.Easing.Quadratic.InOut) // Usar una función de suavizado
-    .start();
-}
-/*function getPointIn3DFromScreenPercentage(xPercentage: number, yPercentage: number): THREE.Vector3 {
-  // Calcular las coordenadas del punto en la pantalla
-  const xScreen = window.innerWidth * xPercentage;
-  const yScreen = window.innerHeight * yPercentage;
-
-  // Convertir las coordenadas de pantalla a coordenadas normalizadas de dispositivo (NDC)
-  mouse.x = (xScreen / window.innerWidth) * 2 - 1;
-  mouse.y = -(yScreen / window.innerHeight) * 2 + 1;
-
-  // Actualizar el raycaster con las coordenadas del mouse y la cámara
-  raycaster.setFromCamera(mouse, camera);
-
-  // Intersecar el rayo con un plano en el espacio 3D
-  const plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-  const pointIn3D = new THREE.Vector3();
-  raycaster.ray.intersectPlane(plane, pointIn3D);
-
-  return pointIn3D;
-}*/
-/*function getPointIn3DFromScreenPixels(xPixel: number, yPixel: number): THREE.Vector3 {
-  mouse.x = (xPixel / window.innerWidth) * 2 - 1;
-  mouse.y = -(yPixel / window.innerHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
-  const pointIn3D = new THREE.Vector3();
-  raycaster.ray.intersectPlane(plane, pointIn3D);
-
-  return pointIn3D;
-}*/
 function InitAudio() {
   // Create a new Audio object
   const audio = new Audio('https://sasiteit.blob.core.windows.net/container-unity/UnityBundles/webgl/ThreeJSProduction/ThreeAvatarFlutter/sounds/' + model[0] + '.mp3');
