2ed446b6865ab84b9cabb5b8c4da3eca609e274f
MarselGarcia <marselord@gmail.com>
2025-05-30 12:04:09 -0500

    -removed bot button -fix problem with fakeId glb (no requieres project name at the end, example: 198-jacuzzis_triptrapp.glb)

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ const tutorial = document.getElementById('tutorial') as HTMLElement;
 const basePath = window.location.pathname.replace(/\/[^/]*$/, '');
 const BASE_URL = `${window.location.origin}${basePath}/models/`;
 const companyId = urlParams.get('fakeId') || urlParams.get('placeId') || "0";
+const fakeId: string | null = urlParams.get('fakeId');
 let mixers: AnimationMixer[] = [];
 // let allAvailableAnimationClipsMap = new Map<Object3D, AnimationClip[]>();
 const clock = new Clock();
@@ -56,26 +57,28 @@ if (window.location.hostname === "localhost") {
   modelUrl = companyId === "0" ? BASE_URL + 'default' + '.glb' : BASE_URL + `${companyId}_${PROJECT.toLowerCase()}.glb`;
 }
 
-if (companyId === "0" || window.location.hostname === "localhost") {
-  GetPlacesFake();
-} else {
-  GetPlacesReal();
+if (fakeId === null) {
+  if (companyId === "0" || window.location.hostname === "localhost") {
+    GetPlacesFake();
+  } else {
+    GetPlacesReal();
+  }
+} else { //start model donwload when fakeID
+  Start();
 }
-//GetPlacesReal(companyId);
 
-
-document.addEventListener('wheel', function(e) {
+document.addEventListener('wheel', function (e) {
   if (e.ctrlKey) {
     e.preventDefault();
   }
 }, { passive: false });
 
-document.addEventListener('gesturestart', function(e) {
+document.addEventListener('gesturestart', function (e) {
   e.preventDefault();
 }, { passive: false });
 
 function Start() {
-  if(ServType === '3'){
+  if (ServType === '3') {
     loader.load(
       modelUrl,
       (gltf) => {
@@ -83,25 +86,24 @@ function Start() {
         gltf.scene.position.set(0, 0, 0);
         scene.add(gltf.scene);
         const { size, center } = GetBoundingBoxSizeAndCenterOfObject(gltf.scene);
-  
+
         const spawn = gltf.scene.getObjectByProperty('name', 'spawn') ||
           gltf.scene.children.find(child => child.name.toLowerCase().includes('spawn'));
-  
+
         if (spawn) {
           // Obtener posición mundial del objeto "spawn"
           const spawnPosition = new Vector3();
           spawn.getWorldPosition(spawnPosition);
-  
+
           // Posiciona la cámara relativa al spawn
           camera.position.set(
             spawnPosition.x + 5,
             spawnPosition.y + 10,
             spawnPosition.z + 0 // puedes ajustar este valor si quieres moverla también en Z
           );
-  
+
           camera.lookAt(spawnPosition);
         } else {
-          console.warn('No se encontró un objeto con nombre que incluya "spawn".');
           camera.position.set(size.x + 10, center.y + size.y + 13, 0);
         }
         //camera.position.set(size.x + 10, center.y + size.y + 13, 0);
@@ -116,7 +118,7 @@ function Start() {
         controls.minZoom = mediam / 75;
         controls.maxZoom = mediam / 2.5;
         controls.update();
-  
+
         //create skybox
         const geometry = new SphereGeometry(1, 60, 40);
         const material = new MeshBasicMaterial({
@@ -128,7 +130,7 @@ function Start() {
         scene.add(backgroundSphere);
         backgroundSphere.scale.set(
           size.length() + 100, size.length() + 100, size.length() + 100);
-  
+
         //populate animation array
         // const mixer = new AnimationMixer(gltf.scene);
         // allAvailableAnimationClipsMap.set(gltf.scene, gltf.animations);
@@ -137,13 +139,13 @@ function Start() {
         //   mixer.clipAction(clip).play();
         // });
         // mixers.push(mixer);
-  
+
         gltf.scene.traverse(child => {
           if (child.name.includes('ROTATE_')) {
             lookAtCamera.push(child);
           }
         });
-  
+
         //populate floorLevels array with the objects that has the following name piso1, piso2, piso3 and so on
         let index = 1;
         while (true) {
@@ -155,40 +157,48 @@ function Start() {
             break;
           }
         }
-  
+
         if (floorLevels.length > 1) {
           initFloorSelector(floorLevels, labelsScene);
         } else {
           document.getElementById('floor-selector-title')!.style.display = 'none';
           document.getElementById('floor-selector')!.style.display = 'none';
         }
-  
+
         const objetivoParent = scene.getObjectByName("objetivos");
         if (objetivoParent) {
           objetivoParent.children.forEach(child => {
             child.visible = false;
           });
         }
-  
+
         loadingscreen.style.display = "none";
         // const navmeshObj = scene.getObjectByName('navmesh');
         // if (navmeshObj) {
         //   createNavMesh(navmeshObj as Mesh)
         // }
+        if (fakeId === null) {
+          SetupPlacesOnScene(places);
 
-        SetupPlacesOnScene(places);
-  
+        } else { //enable ui on fakeId
+          document.getElementById('div3DView')!.style.display = 'block';
+          document.getElementById('search-section')!.style.display = 'none';
+          document.getElementById('footer-button')!.style.visibility = 'hidden';
+          document.getElementById('footer')!.style.display = 'none';
+          document.getElementById('category-selector-parent')!.style.display = 'none';
+        }
+
         //Set selected floor to 1
         const floorSelector = document.getElementById('floor-selector') as HTMLSelectElement;
         floorSelector.selectedIndex = 0;
         const event = new Event('change', { bubbles: true });
         floorSelector.dispatchEvent(event);
-  
+
         GetHTMLElement('#loadingMain').style.display = "none";
       },
       (xhr) => {
         const progress = (xhr.loaded / xhr.total) * 100;
-  
+
         if (loadingBar) {
           loadingBar.style.width = `${progress}%`;
         }
@@ -319,8 +329,8 @@ controls.addEventListener('start', () => {
 
 window.addEventListener('touchend', (event) => {
   //disable raycast 
-  if(shouldBlock(event)) return; 
-  if(contador!== 0) return;
+  if (shouldBlock(event)) return;
+  if (contador !== 0) return;
   const touch = event.changedTouches[0];
   mouse.x = (touch.clientX / window.innerWidth) * 2 - 1;
   mouse.y = -(touch.clientY / window.innerHeight) * 2 + 1;
